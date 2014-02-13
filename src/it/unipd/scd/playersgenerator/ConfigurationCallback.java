5ca997e018d53fa75e82b9a00e6843bd4cb32cbf
Sebastiano Gottardo <dextorer@gmail.com>
2014-02-13 20:38:35 +0100

    added callback to avoid circular dependency through projects

---
+++ src/it/unipd/scd/playersgenerator/ConfigurationCallback.java
@@ -0,0 +1,14 @@
+package it.unipd.scd.playersgenerator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dextor
+ * Date: 2/13/14
+ * Time: 8:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ConfigurationCallback {
+
+    public void onConfigurationConfirmed(String data);
+
+}
